02e5ac8cbccaf1b946f1d4ec2e38cac417e20807
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-03-26 20:53:25 +0530

    h

---
+++ auth/javascript/tut_12_chaiaurcode.js
@@ -0,0 +1,42 @@
+// // Number and Maths in js
+
+
+// // Number in js
+// const score = 400;
+// const balance  = new Number(1000);
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const othernumber = 24.234
+// console.log(othernumber.toPrecision(3));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// // Math Object
+
+//  console.log(Math);
+
+//  console.log(Math.abs(-4));
+//  console.log(Math.abs(4));
+ 
+//  console.log(Math.round(4.7));
+// console.log(Math.round(4.2));
+
+// console.log(Math.ceil(4.1));  // ceil means chappar
+// console.log(Math.ceil(4.9));  // ceil means chappar
+// console.log(Math.floor(4.1));  // floor means neeche
+// console.log(Math.floor(4.9));  // floor means neeche
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+
+
+// const min  = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
