da411c3d8240f743e7e4c4654e39283018c831ca
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-03-26 19:30:22 +0530

    g

---
+++ auth/javascript/tut_9_chaiaurcode.js
@@ -0,0 +1,70 @@
+// Data Types of js
+
+// 1. Primitive Data Types
+// 2. Reference Data Types
+
+// Primitive Data Types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+// console.log("Data Types of js");
+
+// let name = "Harry";
+// let marks = 34.4;
+// let isDriver = true;
+// let nullVar = null;
+// let undef = undefined;
+
+// console.log(name);
+// console.log(marks);
+// console.log(isDriver);
+// console.log(nullVar);
+// console.log(undef);
+
+// console.log(typeof name);
+// console.log(typeof marks);
+// console.log(typeof isDriver);
+// console.log(typeof nullVar);
+// console.log(typeof undef);
+
+
+// Reference Data Types
+// 1. Arrays
+// 2. Object Literals
+// 3. Functions
+// 4. Dates
+
+// Arrays
+// let myarr = [1, 2, 3, 4, false, "string"];
+// console.log(myarr);
+// console.log(typeof myarr);
+
+// Object Literals
+// let stMarks = {
+//     harry: 89,   
+//     Shubham: 36,
+//     RohanDas: 34
+// }
+// console.log(stMarks);
+// console.log(typeof stMarks);
+
+// Functions
+// function findName() {
+
+// }
+// console.log(typeof findName);
+
+
+// const id = Symbol('123');
+// console.log(id);
+// console.log(typeof id);
+
+// const myfunc = function () {
+// console.log("Inside a function");
+// }
+// console.log(myfunc);
+// console.log(typeof myfunc);
