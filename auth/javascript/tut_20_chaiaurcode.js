19affec8538a563ab8fce1b4efb7659d67ada9f6
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-03-27 15:02:47 +0530

    h

---
+++ auth/javascript/tut_20_chaiaurcode.js
@@ -0,0 +1,24 @@
+// Functions with object and array in js
+
+// function add(num1) {
+//    return num1
+// }
+
+// console.log(add(23))
+
+// function cal(...num){
+//     return num
+// }
+
+// console.log(cal(200,4000,2000,567))
+
+const user = {
+    username: "hitesh",
+    price: 200
+};
+
+function handleobject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleobject(user);
