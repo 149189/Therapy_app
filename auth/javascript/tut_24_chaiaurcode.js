ecc6723f4c74b965f808cdafd1ab282133f892c3
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-03-27 15:36:36 +0530

    h

---
+++ auth/javascript/tut_24_chaiaurcode.js
@@ -0,0 +1,14 @@
+// immediately invoked Function Expressions IIFE
+
+
+(function chai(){
+    console.log("Database Connected");
+    
+})();
+
+((name)=>{
+    console.log(`Hello ${name}`);
+    
+})(`Kaustubh`);
+
+
