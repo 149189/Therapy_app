60e50e19efcaeefb2041679a5258aab024faf69e
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-03-27 14:49:37 +0530

    j

---
+++ auth/javascript/tut_19_chaiaurcode.js
@@ -0,0 +1,29 @@
+// Functions and parameters in js
+
+function saymyname() {
+   return console.log("Kaustubh");
+   
+}
+
+// saymyname()
+
+function addtwonumber(a,b) //Parameters 
+{
+    c = a+b;
+    return c
+    
+}
+
+addtwonumber(2,5) // Arguments
+
+const result = addtwonumber(4,5)
+
+// console.log("Result: ", result);
+
+function userlogin(username,password){
+              return console.log(`${username, password} just logged in`);
+               
+
+}
+
+userlogin("Kaustubh", 123)
