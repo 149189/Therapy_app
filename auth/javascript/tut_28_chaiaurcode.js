f6cb2665e0ea2b7c19eaa458d0deb496f20e3859
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-03-28 17:39:45 +0530

    g

---
+++ auth/javascript/tut_28_chaiaurcode.js
@@ -0,0 +1,13 @@
+// While loop and Do while loop
+// for (i = 0; i <= 10; i++) {
+//   while ((i = 2)) {
+//     console.log("Brotha that gyat is big");
+//     break;
+//   }
+//   console.log("Gyat is bigger than mah dih");
+//   break;
+// }
+
+// do {
+
+// } while (condition);
