bebbb608fcc9386510647756462f4ec9722af6d5
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2025-03-28 17:32:36 +0530

    h

---
+++ auth/javascript/tut_27_chaiaurcode.js
@@ -0,0 +1,39 @@
+// For Loop with break and continue
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element==5){
+        // console.log("5 is the best");
+        
+    }
+    // console.log(element);
+    
+    
+}
+
+
+for(let j = 0; j<=5;j++) {
+    for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element==5){
+        // console.log("5 is the best");
+        
+    }
+    // console.log(element);
+    
+    
+}}
+
+
+for(let j = 0; j<=5;j++) {
+    for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element==5){
+        console.log("5 is the best");
+        // continue and break
+        
+    }
+    // console.log(element);
+    
+    
+}}
